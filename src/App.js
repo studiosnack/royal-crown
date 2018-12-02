1cf0b0a2c063f15f5ea65705f9453ce3a1c4afab
Marcos Ojeda <subliminal@gmail.com>
2018-12-02 01:28:00 -0800

    run prettier

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useReducer, useMemo, useEffect, useState } from 'react';
+import React, {useReducer, useMemo, useEffect, useState} from 'react';
 import './App.css';
 
-import { Provider, Client, query, Connect } from 'urql';
+import {Provider, Client, query, Connect} from 'urql';
 import PouchDB from 'pouchdb-browser';
 import debounce from 'lodash/debounce';
 
@@ -22,28 +22,30 @@ const settingsReducer = (state, action) => {
     default:
       return state;
   }
-}
+};
 
 const defaultSettings = {
   _id: 'settings',
   apiKey: '',
   username: '',
   repo: '',
-}
-
+};
 
 const usePouchForSettings = (documentName: string = 'settings') => {
   // TODO: pass reducer as arg
   // TODO: pass default settings as arg?
   // TODO: rename
-  const [settings, dispatchSettings] = useReducer(settingsReducer, defaultSettings);
+  const [settings, dispatchSettings] = useReducer(
+    settingsReducer,
+    defaultSettings,
+  );
 
   useEffect(async () => {
     const storedDocument = await db.get(documentName);
     if (!storedDocument) {
       await db.put(defaultSettings);
-      const dbDoc = await db.get(documentName)
-      dispatchSettings({type: 'init', payload: dbDoc})
+      const dbDoc = await db.get(documentName);
+      dispatchSettings({type: 'init', payload: dbDoc});
     } else {
       dispatchSettings({type: 'init', payload: storedDocument});
     }
@@ -56,42 +58,50 @@ const usePouchForSettings = (documentName: string = 'settings') => {
       const doc = {...latest, ...localSettings};
       await db.put(doc);
     } catch (err) {
-      console.error('there was an error saving to pouch')
+      console.error('there was an error saving to pouch');
       console.error(err || err.stack);
     }
-  }
+  };
 
   const saveState = debounce(_saveState, 2000);
 
   const wrappedDispatch = async (action: *) => {
     const newSettings = settingsReducer(settings, action);
     dispatchSettings(action);
-    saveState(newSettings)
-  }
+    saveState(newSettings);
+  };
   return [settings, wrappedDispatch];
-}
+};
 
 const App = () => {
   // const [settings, dispatchSettings] = useReducer(settingsReducer, defaultSettings);
   const [settings, dispatchSettings] = usePouchForSettings('settings');
 
-  console.log('in component', {settings})
-  const client = useMemo(() => new Client({
-    url: 'https://api.github.com/graphql',
-    fetchOptions: () => {
-      const opts = {headers: {
-        'Content-Type': 'application/json'
-      }};
-      if(settings.apiKey && settings.apiKey.length === 40) {
-        opts.headers = {...opts.headers, Authorization: `Bearer ${settings.apiKey}`};
-      }
-      return opts;
-    }
-  }), [settings.apiKey]);
+  console.log('in component', {settings});
+  const client = useMemo(
+    () =>
+      new Client({
+        url: 'https://api.github.com/graphql',
+        fetchOptions: () => {
+          const opts = {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          };
+          if (settings.apiKey && settings.apiKey.length === 40) {
+            opts.headers = {
+              ...opts.headers,
+              Authorization: `Bearer ${settings.apiKey}`,
+            };
+          }
+          return opts;
+        },
+      }),
+    [settings.apiKey],
+  );
 
   return (
     <div>
-
       <Settings state={settings} dispatch={dispatchSettings} />
 
       <Provider client={client}>
@@ -99,15 +109,41 @@ const App = () => {
       </Provider>
     </div>
   );
-}
+};
 
 const Settings = ({state, dispatch}) => {
-  return <React.Fragment>
-      <label>username: <input value={state.username || ''} onChange={e => dispatch({type: 'username', payload: e.currentTarget.value})}/></label>
-      <label>api-key: <input value={state.apiKey || ''} onChange={e => dispatch({type: 'api-key', payload: e.currentTarget.value})}/></label>
-      <label>repo: <input value={state.repo || ''} onChange={e => dispatch({type: 'repo', payload: e.currentTarget.value})}/></label>
-  </React.Fragment>
-}
+  return (
+    <React.Fragment>
+      <label>
+        username:{' '}
+        <input
+          value={state.username || ''}
+          onChange={e =>
+            dispatch({type: 'username', payload: e.currentTarget.value})
+          }
+        />
+      </label>
+      <label>
+        api-key:{' '}
+        <input
+          value={state.apiKey || ''}
+          onChange={e =>
+            dispatch({type: 'api-key', payload: e.currentTarget.value})
+          }
+        />
+      </label>
+      <label>
+        repo:{' '}
+        <input
+          value={state.repo || ''}
+          onChange={e =>
+            dispatch({type: 'repo', payload: e.currentTarget.value})
+          }
+        />
+      </label>
+    </React.Fragment>
+  );
+};
 
 const reviewQuery = `query ($queryRequested: String!, $queryReviewed: String!) {
   rateLimit {
@@ -150,30 +186,57 @@ fragment prData on SearchResultItemConnection {
 
 `;
 
-const Listing = ({repo, username}) => <Connect query={query(reviewQuery, {
-  queryRequested: `repo:${repo} review-requested:${username} type:pr is:open`,
-  queryReviewed: `repo:${repo} reviewed-by:${username} type:pr is:open`
-})}>
-  {({fetching, loaded, error, data, refetch}) => <div>
-    {fetching && 'fetching...'}
-    {loaded && 'loaded!'}
-    {loaded && <button type="button" onClick={() => refetch({skipCache: true})}>re-fetch data</button>}
-    {loaded && data.requested.nodes.map(pr => <ReviewRequest key={pr.id} pr={pr} username={username} />)}
-  </div>}
-</Connect>
+const Listing = ({repo, username}) => (
+  <Connect
+    query={query(reviewQuery, {
+      queryRequested: `repo:${repo} review-requested:${username} type:pr is:open`,
+      queryReviewed: `repo:${repo} reviewed-by:${username} type:pr is:open`,
+    })}
+  >
+    {({fetching, loaded, error, data, refetch}) => (
+      <div>
+        {fetching && 'fetching...'}
+        {loaded && 'loaded!'}
+        {loaded && (
+          <button type="button" onClick={() => refetch({skipCache: true})}>
+            re-fetch data
+          </button>
+        )}
+        {loaded &&
+          data.requested.nodes.map(pr => (
+            <ReviewRequest key={pr.id} pr={pr} username={username} />
+          ))}
+      </div>
+    )}
+  </Connect>
+);
 
 const ReviewRequest = ({pr, username}) => {
-  const hasReviewed = pr.reviews.nodes.filter(review => review.author.login === username && review.state !== 'COMMENTED').length > 0;
-  const approved = [...pr.reviews.nodes.filter(review => review.author.login === username && review.state === 'APPROVED')].pop();
-  const commented = pr.reviews.nodes.filter(review => review.author.login === username && review.state === 'COMMENTED').length > 0;
-  return <div>
-    {!hasReviewed && '🚨'}
-    {approved && '✅'}
-    {commented && '💬'}
-    <a target="_blank" rel="noopener noreferrer" href={pr.url}>#{[...pr.url.split('/')].pop()} - {pr.author.login} - {pr.title}</a>
-  </div>
-}
+  const hasReviewed =
+    pr.reviews.nodes.filter(
+      review =>
+        review.author.login === username && review.state !== 'COMMENTED',
+    ).length > 0;
+  const approved = [
+    ...pr.reviews.nodes.filter(
+      review => review.author.login === username && review.state === 'APPROVED',
+    ),
+  ].pop();
+  const commented =
+    pr.reviews.nodes.filter(
+      review =>
+        review.author.login === username && review.state === 'COMMENTED',
+    ).length > 0;
+  return (
+    <div>
+      {!hasReviewed && '🚨'}
+      {approved && '✅'}
+      {commented && '💬'}
+      <a target="_blank" rel="noopener noreferrer" href={pr.url}>
+        #{[...pr.url.split('/')].pop()} - {pr.author.login} - {pr.title}
+      </a>
+    </div>
+  );
+};
 
 export default App;
-
-
