8d6e84a7939d879118576d8f5e3d54e4e978e623
Marcos Ojeda <subliminal@gmail.com>
2019-04-16 09:45:23 -0700

    Updates

---
+++ precache-manifest.8cd53a8f7f3c7659057e77f6495af036.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "40c1cc76b7b32b646b19",
+    "url": "/royal-crown/static/css/main.d0058167.chunk.css"
+  },
+  {
+    "revision": "40c1cc76b7b32b646b19",
+    "url": "/royal-crown/static/js/main.40c1cc76.chunk.js"
+  },
+  {
+    "revision": "e737dc29ce6116af1e98",
+    "url": "/royal-crown/static/js/1.e737dc29.chunk.js"
+  },
+  {
+    "revision": "c94f8a3311235cc826d3",
+    "url": "/royal-crown/static/js/runtime~main.c94f8a33.js"
+  },
+  {
+    "revision": "5f67bb9d098c409d6c3dcbcc8835f5e0",
+    "url": "/royal-crown/index.html"
+  }
+];
